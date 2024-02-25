d06b44a496251b161607ac1234965bb88c09c17c
Thuận NM <150407855+kenduNMT@users.noreply.github.com>
2024-02-25 22:20:56 +0700

    Add files via upload

---
+++ ss23/bt1.js
@@ -0,0 +1,4 @@
+var num= Number(prompt("nhập vào số nguyên dương"))
+for(var i=1; i <= num; i++ ){
+    console.log(+i)
+}
---
+++ ss23/bt2.js
@@ -0,0 +1,8 @@
+var num = Number(prompt("Nhập vào số nguyên dương"));
+var hi = 1;
+
+for (var i = 1; i <= num; i++) {
+   hi *= i;
+}
+
+console.log(hi);
