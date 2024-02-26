f2e81829bd6945ecdf5773a0ffa7d97b941819f5
Thuận NM <150407855+kenduNMT@users.noreply.github.com>
2024-02-26 08:56:30 +0700

    Add files via upload

---
+++ bttrenlop/index.js
@@ -0,0 +1,21 @@
+function fibonacci(n) {
+    let a = 0, b = 1;
+    if (n <= 0) {
+        return "Vui lòng nhập một số nguyên dương.";
+    } else if (n == 1) {
+        return a;
+    } else if (n == 2) {
+        return b;
+    } else {
+        let c;
+        for(let i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
+let n = prompt("Nhập vào số nguyên dương n:");
+console.log("Số Fibonacci thứ " + n + " là: " + fibonacci(n));
